3ca8bc68a01c6aabd2d205be9932db8204c5acb4
20161104574 <2460779735@qq.com>
2017-07-02 20:06:51 +0800

    the 6
    动刀继续

--- qss/main.c
+++ qss/main.c
@@ -15,45 +15,45 @@ int main(int argc, const char * argv[])
     FILE *fw;
     char str1[500];
     char str2[500];
-    char date[50],lat[50],sorn[50],lon[50],eorw[50],spe[50],cor[50],alt[50];
+    char year[50],mon[50],day[50],time[50],min[50],sec[50],lat[50],sorn[50],lon[50],eorw[50],spe[50],cor[50],alt[50],num[50];
     int i;
-    fr=fopen("//Users//20161104574al//desktop//GPS170510.log","r+");
-    fw=fopen("//Users//20161104574al//desktop//output.csv","w+");
+    fr=fopen("//Users//20161104574al//desktop//qss//GPS170510.log","r+");
+    fw=fopen("//Users//20161104574al//desktop//qss//output.csv","w+");
     
-    fprintf(fw,"时间,维度,南北维,经度,东西经,地面速率,航向,海拔\n");
+    fprintf(fw,"日期：年,月,日,时,分,秒,维度（单位：度）,南北维,经度（单位：度）,东西经,地面速率（单位：节）,航向（单位：度）,海拔（单位：米）,卫星数量（单位：个）\n");
     while(fscanf(fr,"%s%s",str1,str2)!=EOF)
     {
         printf("结果:%s%s\n",str1,str2);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+55];
-        date[2]='\0';
-        printf("时间：%s年",date);
+            year[i]=str1[i+55];
+        year[2]='\0';
+        printf("时间：%s年",year);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+53];
-        date[2]='\0';
-        printf("%s月",date);
+            mon[i]=str1[i+53];
+        mon[2]='\0';
+        printf("%s月",mon);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+51];
-        date[2]='\0';
-        printf("%s日",date);
+            day[i]=str1[i+51];
+        day[2]='\0';
+        printf("%s日",day);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+7];
-        date[2]='\0';
-        printf("%s时",date);
+            time[i]=str1[i+7];
+        time[2]='\0';
+        printf("%s时",time);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+9];
-        date[2]='\0';
-        printf("%s分",date);
+            min[i]=str1[i+9];
+        min[2]='\0';
+        printf("%s分",min);
         
         for(i=0;i<2;i++)
-            date[i]=str1[i+11];
-        date[2]='\0';
-        printf("%s秒\t",date);
+            sec[i]=str1[i+11];
+        sec[2]='\0';
+        printf("%s秒\t",sec);
         
         
         for(i=0;i<8;i++)
@@ -93,10 +93,16 @@ int main(int argc, const char * argv[])
         for(i=0;i<4;i++)
             alt[i]=str2[i+43];
         alt[4]='\0';
-        printf("海拔:%s米\n",alt);
+        printf("海拔:%s米\t",alt);
         
         
-        fprintf(fw,"%s,%s,%s,%s,%s,%s,%s,%s\n",date,lat,sorn,lon,eorw,spe,cor,alt);
+        for(i=0;i<2;i++)
+            num[i]=str2[i+39];
+        num[2]='\0';
+        printf("卫星个数:%s个\n",num);
+        
+        
+        fprintf(fw,"20%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",year,mon,day,time,min,sec,lat,sorn,lon,eorw,spe,cor,alt,num);
         
         
     }
